f07c237478560fb895626c7a18e2477c46508431
Kulakoff <kulakov.v.n@ya.ru>
2022-06-12 22:15:35 +0300

    commit 2
    mvc work

---
+++ MVVM-2/MVVM-2/Model/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  MVVM-2
+//
+//  Created by Владимир  on 12.06.22.
+//
+
+import Foundation
+
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
